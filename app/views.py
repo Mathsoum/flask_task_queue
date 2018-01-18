0432dd75a9d9e5cbe163179658ef0216f652c5a3
Mathieu SOUM <mathieu.soum@aboard-eng.com>
2018-01-18 09:19:15 +0100

    Reload only when needed. Needs reworks with the flag.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 import flask
 from flask_login import login_user
+import json
 
-from app import app, tasks, full_task_list
+import app
+from app import app as _app, tasks, full_task_list
 from app.form import TaskForm, LoginForm
 from app.runner import Task
 from app.user import User
@@ -9,12 +11,12 @@ from app.user import User
 import url
 
 
-@app.route('/')
+@_app.route('/')
 def hello_world():
     return flask.render_template("main.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@_app.route('/login', methods=['GET', 'POST'])
 def login():
     # Here we use a class of some kind to represent and validate our
     # client-side form data. For example, WTForms is a library that will
@@ -38,23 +40,31 @@ def login():
     return flask.render_template('login.html', form=form)
 
 
-@app.route('/add', methods=['POST', 'GET'])
+@_app.route('/add', methods=['POST', 'GET'])
 def add_task():
     if flask.request.method == 'POST':
         if flask.request.form['message'] != '':
             tasks.put(Task(flask.request.form['message']))
 
     form = TaskForm()
+    app.needs_reload = True
     return flask.render_template("addition.html", form=form, full_task_list=full_task_list)
 
 
-@app.route('/_table')
+@_app.route('/_table')
 def task_table():
     return flask.render_template("task_table.html", full_task_list=full_task_list)
 
 
-@app.route('/_add_numbers')
+@_app.route('/_add_numbers')
 def add_numbers():
     a = flask.request.args.get('a', 0, type=int)
     b = flask.request.args.get('b', 0, type=int)
     return flask.jsonify(result=a + b)
+
+
+@_app.route('/_needs_reload')
+def needs_reload():
+    return_value = app.needs_reload
+    app.needs_reload = False
+    return json.dumps({"needs_reload": return_value})
